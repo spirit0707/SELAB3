8f1ea6a32ee8e3f4cc1668ce6f6e34fab5eb6a54
spirit <82125973+spirit0707@users.noreply.github.com>
2024-04-05 23:44:49 +0300

    Update server.cs

--- Server/server.cs
+++ Server/server.cs
@@ -1,92 +1,277 @@
-﻿using System;
+using System;
+using System.Diagnostics.Metrics;
+using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 
-IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
-using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-socket.Bind(ipPoint);
-socket.Listen(1000);
-Console.WriteLine("Server started!");
-while (true)
+namespace Server
 {
-    using Socket client = await socket.AcceptAsync();
-    var responseBytes = new byte[512];
-    var bytes = await client.ReceiveAsync(responseBytes);
-    string response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
-    if (response == "exit")
+    internal class Program
     {
-        break;
-    }
-    string[] clientArgs = response.Split(' ', 3);
-    string message = "";
-    string strExeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-    string strWorkPath = Path.GetDirectoryName(strExeFilePath);
-    string dir = Path.Combine(strWorkPath, "server\\data");
-    Directory.CreateDirectory(dir);
-    var filename = Path.Combine(dir, clientArgs[1]);
+        private static ReaderWriterLockSlim _lock = new();
+        static Dictionary<int, string> dict = {};
+        static int counter = 0;
+        static string dir = "";
 
-    if (clientArgs[0] == "PUT")
-    {
-        if (File.Exists(filename))
+        static void Main()
         {
-            message += "403";
-        }
-        else
-        {
-            try
+            if (File.Exists("data.txt"))
             {
-                using StreamWriter writer = new(filename);
-                writer.Write(clientArgs[2]);
-                message += "200";
+                var lines = File.ReadLines("data.txt");
+                foreach (var line in lines)
+                {
+                    var data = line.Split(' ');
+                    dict.Add(int.Parse(data[0]), data[1]);
+                    counter = Math.Max(counter, int.Parse(data[0]));
+                }
             }
-            catch
+            string strExeFilePath = Assembly.GetExecutingAssembly().Location;
+            string strWorkPath = Path.GetDirectoryName(strExeFilePath);
+            dir = Path.Combine(strWorkPath, "server\\data");
+            Directory.CreateDirectory(dir);
+
+            IPEndPoint ipPoint = new(IPAddress.Any, 8888);
+            using Socket socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(1000);
+            Console.WriteLine("Server started!");
+
+            while (true)
             {
-                message += "403";
+                Socket client = socket.Accept();
+                new Thread(async () => await Interaction(client)).Start();
+            }
+        }
+        static async Task Interaction(Socket client)
+        {
+            var responseBytes = new byte[1024];
+            var bytes = await client.ReceiveAsync(responseBytes);
+            string response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+            if (response == "exit")
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                Environment.Exit(0);
             }
 
-        }
-    }
-    else if (clientArgs[0] == "DELETE")
-    {
-        if (File.Exists(filename))
-        {
-            try
-            {
-                File.Delete(filename);
-                message += "200";
-            }
-            catch
-            {
-                message += "404";
-            }
-        }
-        else
-        {
-            message += "404";
-        }
+            string[] clientArgs = response.Split(' ', 2);
+            string message = "";
+            using MemoryStream ms = new();
 
-    }
-    else
-    {
-        if (File.Exists(filename))
-        {
-            try
+            if (clientArgs[0] == "PUT")
             {
-                string content = File.ReadAllText(filename);
-                message += "200 " + content;
+                string filename;
+                if (string.IsNullOrEmpty(clientArgs[1]))
+                {
+                    filename = Path.Combine(dir, Guid.NewGuid().ToString());
+                }
+                else
+                {
+                    filename = Path.Combine(dir, clientArgs[1]);
+                }
+                if (File.Exists(filename))
+                {
+                    message += "403";
+                }
+                else
+                {
+                    try
+                    {
+                        var buffer = new byte[512];
+                        int b;
+                        do
+                        {
+                            b = await client.ReceiveAsync(buffer);
+                            ms.Write(buffer, 0, b);
+
+                        } while (b > 0);
+
+
+                        _lock.EnterWriteLock();
+                        try
+                        {
+                            using (FileStream fs = new(filename, FileMode.CreateNew, FileAccess.Write))
+                            {
+                                ms.WriteTo(fs);
+                            }
+
+                            counter++;
+                            dict.Add(counter, filename);
+
+                            using (StreamWriter sw = File.AppendText("data.txt"))
+                            {
+                                sw.WriteLine(counter + " " + filename);
+                            }
+
+                            message += "200 " + counter;
+                        }
+                        catch
+                        {
+                            message += "403";
+                        }
+                        finally { _lock.ExitWriteLock(); }
+                    }
+                    catch
+                    {
+                        message += "404";
+                    }
+
+                }
             }
-            catch
+            else if (clientArgs[0] == "DELETE")
             {
-                message += "404";
+                var deleteArgs = clientArgs[1].Split(' ');
+
+                string filename;
+                if (deleteArgs[0] == "BY_ID")
+                {
+                    _lock.EnterUpgradeableReadLock();
+                    try
+                    {
+                        if (dict.ContainsKey(int.Parse(deleteArgs[1])))
+                        {
+                            filename = dict[int.Parse(deleteArgs[1])];
+                            _lock.EnterWriteLock();
+                            try
+                            {
+                                File.Delete(filename);
+
+                                List<string> lines = new(File.ReadAllLines("data.txt"));
+                                List<string> newlines = [];
+                                foreach (string line in lines)
+                                {
+                                    var temp = line.Split(' ');
+                                    if (temp[1] != filename)
+                                    {
+                                        newlines.Add(line);
+                                    }
+                                    else
+                                    {
+                                        dict.Remove(int.Parse(temp[0]));
+                                    }
+                                }
+
+                                File.WriteAllLines("data.txt", newlines);
+
+                                message += "200";
+                            }
+                            catch
+                            {
+                                message += "404";
+                            }
+                            finally { _lock.ExitWriteLock(); }
+                        }
+                        else
+                        {
+                            message += "404";
+                        }
+                    }
+                    finally { _lock.ExitUpgradeableReadLock(); }
+                }
+                else 
+                {
+                    filename = Path.Combine(dir, deleteArgs[1]);
+                    if (File.Exists(filename))
+                    {
+                        _lock.EnterWriteLock();
+                        try
+                        {
+                            File.Delete(filename);
+
+                            List<string> lines = new(File.ReadAllLines("data.txt"));
+                            List<string> newlines = [];
+                            foreach (string line in lines)
+                            {
+                                var temp = line.Split(' ');
+                                if (temp[1] != filename)
+                                {
+                                    newlines.Add(line);
+                                }
+                                else
+                                {
+                                    dict.Remove(int.Parse(temp[0]));
+                                }
+                            }
+
+                            File.WriteAllLines("data.txt", newlines);
+
+                            message += "200";
+                        }
+                        catch
+                        {
+                            message += "404";
+                        }
+                        finally { _lock.ExitWriteLock(); }
+                    }
+                    else
+                    {
+                        message += "404";
+                    }
+                }
             }
-        }
-        else
-        {
-            message += "404";
+            else 
+            {
+                var getArgs = clientArgs[1].Split(' ');
+                string filename;
+                if (getArgs[0] == "BY_ID")
+                {
+                    _lock.EnterReadLock();
+                    try
+                    {
+                        if (dict.ContainsKey(int.Parse(getArgs[1])))
+                        {
+                            filename = dict[int.Parse(getArgs[1])];
+                            ms.Write(File.ReadAllBytes(filename));
+                            message += "200";
+                        }
+                        else
+                        {
+                            message += "404";
+                        }
+                    }
+                    catch
+                    {
+                        message += "404";
+                    }
+                    finally { _lock.ExitReadLock(); }
+                }
+                else 
+                {
+                    filename = Path.Combine(dir, getArgs[1]);
+                    if (File.Exists(filename))
+                    {
+                        _lock.EnterReadLock();
+                        try
+                        {
+                            ms.Write(File.ReadAllBytes(filename));
+                            message += "200";
+                        }
+                        catch
+                        {
+                            message += "404";
+                        }
+                        finally { _lock.ExitReadLock(); }
+                    }
+                    else
+                    {
+                        message += "404";
+                    }
+                }
+                message += " ";
+            }
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            if (clientArgs[0] == "GET")
+            {
+                await client.SendAsync(messageBytes.Concat(ms.ToArray()).ToArray());
+            }
+            else
+            {
+                await client.SendAsync(messageBytes);
+            }
+            client.Shutdown(SocketShutdown.Send);
         }
     }
-    var messageBytes = Encoding.UTF8.GetBytes(message);
-    await client.SendAsync(messageBytes);
 }
-socket.Close();
